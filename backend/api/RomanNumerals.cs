e0fc440d853d9c7e0e80696348ddabcb6b2db236
Tomasz Mikus <mikus.tomasz@gmail.com>
2019-02-23 09:04:17 +0000

    Implement the algorithm to convert from roman numerals to arabic.

--- backend/api/RomanNumerals.cs
+++ backend/api/RomanNumerals.cs
@@ -8,10 +8,48 @@ namespace api
         {
             throw new NotImplementedException();
         }
+
+        private static int GetRomanNumeralValue(char numeral)
+        {
+            switch (numeral)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default:
+                    throw new ArgumentException($"Invalid roman numeral supplied: {numeral}");
+            }
+        }
         
         public static int ToArabic(string numeral)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            for (var index = 0; index < numeral.Length; index++)
+            {
+                var currentSymbol = GetRomanNumeralValue(numeral[index]);
+                if (index + 1 >= numeral.Length)
+                {
+                    result += currentSymbol;
+                    break;
+                }
+
+                var nextSymbol = GetRomanNumeralValue(numeral[index + 1]);
+                if (currentSymbol >= nextSymbol)
+                {
+                    result += currentSymbol;
+                }
+                else
+                {
+                    result += nextSymbol - currentSymbol;
+                    index += 1; // Manually advance the index as we've consumed two tokens in one turn
+                }
+            }
+
+            return result;
         }
     }
 }

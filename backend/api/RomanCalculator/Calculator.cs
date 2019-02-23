d1ea482b919123565e69abb15543b8da128093ef
Tomasz Mikus <mikus.tomasz@gmail.com>
2019-02-23 13:46:21 +0000

    Implement the roman calculator.

--- backend/api/RomanCalculator/Calculator.cs
+++ backend/api/RomanCalculator/Calculator.cs
@@ -4,9 +4,15 @@ namespace api.RomanCalculator
 {
     public class Calculator
     {
+        private static Interpreter _interpreter = new Interpreter();
+        
         public static string Evaluate(string input)
         {
-            throw new NotImplementedException();
+            var scanner = new Scanner(input);
+            var tokens = scanner.ScanTokens();
+            var parser = new Parser(tokens);
+            var expression = parser.Parse();
+            return _interpreter.Interpret(expression);
         }
     }
 }
